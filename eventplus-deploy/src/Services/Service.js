9e8972049337a16497066f3efcd570616461498c
GabrielVictor0 <bielgvsa@gmail.com>
2023-12-11 10:56:55 -0300

    .

--- eventplus-deploy/src/Services/Service.js
+++ eventplus-deploy/src/Services/Service.js
@@ -42,7 +42,7 @@ export const loginResource = '/Login';
 
 // const apiPort = '5000';
 // const localApiUri = `http://localhost:${apiPort}/api`;
-const externallApiUri = `https://eventwebapi-gabrielvictor.azurewebsites.net`;
+const externallApiUri = `https://eventwebapi-gabrielvictor.azurewebsites.net/api`;
 // const externalApiUri = null;
 
 const api = axios.create({
