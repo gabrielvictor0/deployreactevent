56cc8d9bd59ba55225174994dc4e39fae4b51fe7
GabrielVictor0 <bielgvsa@gmail.com>
2023-12-13 10:07:28 -0300

    .

--- eventplus-deploy/src/components/NextEvent/NextEvent.jsx
+++ eventplus-deploy/src/components/NextEvent/NextEvent.jsx
@@ -5,7 +5,6 @@ import { Tooltip } from "react-tooltip";
 
 // importar a função lá do arquivo stringFunction (destructuring)
 import { dateFormatDbToView } from "../../Utils/stringFunctions";
-import Title from "../Title/Title";
 
 const NextEvent = ({ title, description, eventDate, idEvent }) => {
   function conectar(idEvent) {
@@ -13,36 +12,34 @@ const NextEvent = ({ title, description, eventDate, idEvent }) => {
     alert(`Chamar o recurso para conectar: ${idEvent}`);
   }
   return (
-    <>
-    <Title titleText={"Proximos Eventos"} />
-      <article className="event-card">
-        <h2 className="event-card__title">{title}</h2>
+    <article className="event-card">
+      <h2 className="event-card__title">{title}</h2>
 
-        <p
-          className="event-card__description"
-          data-tooltip-id={idEvent}
-          data-tooltip-content={description}
-          data-tooltip-place="top"
-        >
-          <Tooltip id={idEvent} className="tooltip" />
-          {description.substr(0, 15)} ...
-        </p>
+      <p
+        className="event-card__description"
+        
+        data-tooltip-id={idEvent}
+        data-tooltip-content={description}
+        data-tooltip-place="top"
+      >
+        <Tooltip id={idEvent} className="tooltip" />
+        {description.substr(0, 15)} ...
+      </p>
 
-        <p className="event-card__description">
-          {/* aplicar a função pra converter a data */}
-          {dateFormatDbToView(eventDate)}
-        </p>
+      <p className="event-card__description">
+        {/* aplicar a função pra converter a data */}
+        {dateFormatDbToView(eventDate)}
+      </p>
 
-        <a
-          onClick={() => {
-            conectar(idEvent);
-          }}
-          className="event-card__connect-link"
-        >
-          Conectar
-        </a>
-      </article>
-    </>
+      <a
+        onClick={() => {
+          conectar(idEvent);
+        }}
+        className="event-card__connect-link"
+      >
+        Conectar
+      </a>
+    </article>
   );
 };
 
--- eventplus-deploy/src/pages/HomePage/HomePage.jsx
+++ eventplus-deploy/src/pages/HomePage/HomePage.jsx
@@ -54,7 +54,7 @@ const HomePage = () => {
       {/* PRÓXIMOS EVENTOS */}
       <section className="proximos-eventos">
         <Container>
-          {/* <Title titleText={"Próximos Eventos"} /> */}
+          <Title titleText={"Próximos Eventos"} />
 
           <div className="events-box">
             {nextEvents.map((e) => {
