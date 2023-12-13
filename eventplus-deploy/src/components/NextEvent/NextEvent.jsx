c7de447002bb94ab9101aa21f128815184814b22
GabrielVictor0 <bielgvsa@gmail.com>
2023-12-13 10:06:17 -0300

    .

--- eventplus-deploy/src/components/NextEvent/NextEvent.jsx
+++ eventplus-deploy/src/components/NextEvent/NextEvent.jsx
@@ -13,35 +13,36 @@ const NextEvent = ({ title, description, eventDate, idEvent }) => {
     alert(`Chamar o recurso para conectar: ${idEvent}`);
   }
   return (
-    <article className="event-card">
-      <Title titleText={"Proximos Eventos"}/>
-      <h2 className="event-card__title">{title}</h2>
+    <>
+    <Title titleText={"Proximos Eventos"} />
+      <article className="event-card">
+        <h2 className="event-card__title">{title}</h2>
 
-      <p
-        className="event-card__description"
-        
-        data-tooltip-id={idEvent}
-        data-tooltip-content={description}
-        data-tooltip-place="top"
-      >
-        <Tooltip id={idEvent} className="tooltip" />
-        {description.substr(0, 15)} ...
-      </p>
+        <p
+          className="event-card__description"
+          data-tooltip-id={idEvent}
+          data-tooltip-content={description}
+          data-tooltip-place="top"
+        >
+          <Tooltip id={idEvent} className="tooltip" />
+          {description.substr(0, 15)} ...
+        </p>
 
-      <p className="event-card__description">
-        {/* aplicar a função pra converter a data */}
-        {dateFormatDbToView(eventDate)}
-      </p>
+        <p className="event-card__description">
+          {/* aplicar a função pra converter a data */}
+          {dateFormatDbToView(eventDate)}
+        </p>
 
-      <a
-        onClick={() => {
-          conectar(idEvent);
-        }}
-        className="event-card__connect-link"
-      >
-        Conectar
-      </a>
-    </article>
+        <a
+          onClick={() => {
+            conectar(idEvent);
+          }}
+          className="event-card__connect-link"
+        >
+          Conectar
+        </a>
+      </article>
+    </>
   );
 };
 
